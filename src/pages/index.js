2879d71ff279d9eeb81fb4fb7ff26ed88af20d99
patryk-pi <patryk.piasecki1@gmail.com>
2023-04-18 10:05:59 +0200

    setting up graphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,47 @@
-const Home = () => {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+import {gql, GraphQLClient} from 'graphql-request';
+
+export const getStaticProps = async () => {
+    const url = process.env.ENDPOINT
+
+    const graphQLClient = new GraphQLClient(url, {
+        headers: {
+            "Authorizaition": process.env.HYGRAPH_TOKEN
+        }
+    });
+
+    const query = gql`
+    query {
+        videos {
+            id,
+            title, 
+            description, 
+            seen, 
+            slug, 
+            tags,
+            thumbnail {
+                url
+            }
+        }
+    }`
+
+    const data = await graphQLClient.request(query)
+    const videos = data.videos;
+
+    return {
+        props: {
+            videos
+        }
+    }
+}
+
+
+const Home = ({videos}) => {
+    console.log(videos)
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    )
 }
 
 export default Home
